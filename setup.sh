8bdc8a23d4233e3d439d46bae26522e10481bb04
tokenChains <140876895+tokenChains@users.noreply.github.com>
2024-06-21 15:22:31 +1000

    Error debugged \& fixed

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ sudo mkdir -p /etc/dmswitch
 sudo cp -r config/hooks /etc/dmswitch
 sudo cp config/config.toml /etc/dmswitch
 sudo cp config/dmswitch.service /etc/systemd/system
+sudo systemctl enable dmswitch.service
+sudo systemctl start dmswitch.service
